dff40c38146f5d686e287a3ce2635e61d0988ad3
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-02-07 09:46:00 -0500

    upload project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,114 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const colors = {
+  used: "lightgreen",
+  selected: "deepskyblue",
+  wrong: "lightcoral"
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Number extends React.PureComponent {
+  bgColor() {
+    const { used, selected, wrong } = this.props;
+    if (used) {
+      return { background: colors.used };
+    }
+    if (selected) {
+      return wrong
+        ? { background: colors.wrong }
+        : { background: colors.selected };
+    }
+    return {};
+  }
+
+  handleClick(num) {
+    if (!this.props.used) {
+      this.props.onClick(num);
+    }
+  }
+
+  render() {
+    const { num } = this.props;
+    return (
+      <button
+        style={this.bgColor()}
+        onClick={() => this.handleClick(num)}
+        className="number"
+      >
+        {this.props.num}
+      </button>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: Math.floor(Math.random() * 9) + 1,
+      selectedNumbers: [],
+      usedNumbers: []
+    };
+  }
+
+  handleClick(num) {
+    this.setState(prevState => {
+      let { stars, selectedNumbers, usedNumbers } = prevState;
+      selectedNumbers = [...selectedNumbers, num];
+      const sum = selectedNumbers.reduce((acc, cv) => (acc += cv), 0);
+
+      if (sum === stars) {
+        usedNumbers = [...usedNumbers, ...selectedNumbers];
+        selectedNumbers = [];
+        stars = Math.floor(Math.random() * 9) + 1;
+      }
+
+      return { selectedNumbers, usedNumbers, stars };
+    });
+  }
+
+  render() {
+    const { usedNumbers, selectedNumbers, stars } = this.state;
+    const selectedSum = selectedNumbers.reduce((acc, cv) => (acc += cv), 0);
+    const selectionIsWrong = selectedSum > stars;
+    const arr = [...Array(9).keys()];
+    const starsArray = [...Array(stars).keys()];
+    return (
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum to the number of stars
+        </div>
+        <div className="body">
+          <div className="stars">
+            {starsArray.map(v => {
+              return <div key={v} className="star" />;
+            })}
+          </div>
+          <div className="play-numbers">
+            {arr.map(v => {
+              const isSelected = selectedNumbers.find(i => i === v + 1)
+                ? true
+                : false;
+              const isUsed = usedNumbers.find(i => i === v + 1) ? true : false;
+              return (
+                <Number
+                  used={isUsed}
+                  selected={isSelected}
+                  key={v}
+                  onClick={i => this.handleClick(i)}
+                  wrong={selectionIsWrong}
+                  num={v + 1}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
